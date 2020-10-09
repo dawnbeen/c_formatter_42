8d41af25db4440db6e19e6fac52b4acaef20efd6
Charles Cabergs <me@cacharle.xyz>
2020-10-09 20:07:54 +0200

    Fixing #2: Condition aligned with spaces

--- src/run.py
+++ src/run.py
@@ -6,7 +6,7 @@
 #    By: cacharle <me@cacharle.xyz>                 +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/10/09 18:15:09 by cacharle          #+#    #+#              #
-#    Updated: 2020/10/09 18:41:29 by cacharle         ###   ########.fr        #
+#    Updated: 2020/10/09 20:02:49 by charles          ###   ########.fr        #
 #                                                                              #
 # ############################################################################ #
 
@@ -34,9 +34,20 @@ def space_before_semi_colon(content: str) -> str:
     )
 
 
+def remove_multiline_condition_space(content: str) -> str:
+    return re.sub(
+        r"(?P<tabs>\t+) {1,3}(?P<rest>.*)",
+        lambda match: "{}\t{}".format(
+            match.group("tabs"),
+            match.group("rest")),
+        content
+    )
+
+
 def run_all(content: str) -> str:
     """ Run all formatters """
     content = clang_format(content)
+    content = remove_multiline_condition_space(content)
     content = parenthesize_return(content)
     content = space_before_semi_colon(content)
     content = hoist(content)
--- src/test/test_run.py
+++ src/test/test_run.py
@@ -6,11 +6,11 @@
 #    By: cacharle <me@cacharle.xyz>                 +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/10/09 18:15:56 by cacharle          #+#    #+#              #
-#    Updated: 2020/10/09 18:41:48 by cacharle         ###   ########.fr        #
+#    Updated: 2020/10/09 20:03:42 by charles          ###   ########.fr        #
 #                                                                              #
 # ############################################################################ #
 
-from run import parenthesize_return, space_before_semi_colon
+from run import parenthesize_return, space_before_semi_colon, remove_multiline_condition_space
 
 
 def test_run_space_before_semi_colon():
@@ -31,3 +31,40 @@ def test_run_parenthesize_return():
     assert parenthesize_return("return \n\nfoo();") == "return (foo());"
     assert parenthesize_return("return foo()\n\n;") == "return (foo());"
     assert parenthesize_return("return \nfoo()\n;") == "return (foo());"
+
+
+def test_run_space_in_condition():
+    input = """
+while (input != NULL && input->tag & TAG_IS_STR &&
+\t   input->tag & TAG_STICK &&
+\t   input->next != NULL && input->next->tag & TAG_IS_STR)
+    """
+    output = """
+while (input != NULL && input->tag & TAG_IS_STR &&
+\t\tinput->tag & TAG_STICK &&
+\t\tinput->next != NULL && input->next->tag & TAG_IS_STR)
+    """
+    assert remove_multiline_condition_space(input) == output
+
+    input = """
+while (input != NULL && input->tag & TAG_IS_STR &&
+\t  input->tag & TAG_STICK &&
+\t  input->next != NULL && input->next->tag & TAG_IS_STR)
+    """
+    output = """
+while (input != NULL && input->tag & TAG_IS_STR &&
+\t\tinput->tag & TAG_STICK &&
+\t\tinput->next != NULL && input->next->tag & TAG_IS_STR)
+    """
+    assert remove_multiline_condition_space(input) == output
+    input = """
+while (input != NULL && input->tag & TAG_IS_STR &&
+\t input->tag & TAG_STICK &&
+\t input->next != NULL && input->next->tag & TAG_IS_STR)
+    """
+    output = """
+while (input != NULL && input->tag & TAG_IS_STR &&
+\t\tinput->tag & TAG_STICK &&
+\t\tinput->next != NULL && input->next->tag & TAG_IS_STR)
+    """
+    assert remove_multiline_condition_space(input) == output
