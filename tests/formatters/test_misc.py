132c4ff7760bc24539ebb87241db0abbdb07cc0a
Younes Aassila <47226184+younesaassila@users.noreply.github.com>
2022-11-14 13:21:47 +0100

    🐛 Fix #37

--- tests/formatters/test_misc.py
+++ tests/formatters/test_misc.py
@@ -101,5 +101,6 @@ def test_insert_void():
     assert "int\tmain(void)" == insert_void("int\tmain()")
     assert "void\tfunc  (void)" == insert_void("void\tfunc  ( )")
     assert "void\t\tfunc  (void)" == insert_void("void\t\tfunc  ( )")
+    assert "void\t\t*func  (void)" == insert_void("void\t\t*func  ( )")
     assert "\t(void *)foo()" == insert_void("\t(void *)foo()")
     assert "\tfoo()" == insert_void("\tfoo()")
