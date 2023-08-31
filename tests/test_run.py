9127f20695b171cb8081b67761185a8642c03e71
Younes Aassila <47226184+younesaassila@users.noreply.github.com>
2023-08-31 14:50:31 +0200

    Fixed test string

--- tests/test_run.py
+++ tests/test_run.py
@@ -90,7 +90,7 @@ def test_function_call_in_comment():
 #include "libft.h"
 
 /*
-The  bzero()  function  erases  the data in the n bytes of the memory starting at the location pointed to by s, by writing zeros (bytes containing '\0') to that area.
+The  bzero()  function  erases  the data in the n bytes of the memory starting at the location pointed to by s, by writing zeros (bytes containing '\\0') to that area.
 
 The explicit_bzero() function performs the same task as bzero().  It differs from bzero() in  that  it  guarantees that compiler optimizations will not remove the erase operation if the compiler deduces that the operation is "un-necessary".
 */
